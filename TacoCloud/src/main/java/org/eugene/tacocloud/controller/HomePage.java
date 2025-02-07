4edaef2ec0bb344b6ec8175d0b6ba9df5b0c9c26
ІО-13 Євген Міхолат <miholatevgen@gmail.com>
2025-02-07 10:58:27 +0200

    init commit

---
+++ TacoCloud/src/main/java/org/eugene/tacocloud/controller/HomePage.java
@@ -0,0 +1,17 @@
+package org.eugene.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomePage {
+
+
+    @GetMapping()
+    public String init(){
+        return "home";
+    }
+
+}
